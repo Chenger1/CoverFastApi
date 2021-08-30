ddc1ed17cc91f7ed07674aad0b0e7f204d5598ba
Chenger1 <exs2199@gmail.com>
2021-08-31 00:05:39 +0300

    Handler and page to edit main page text

--- app/db/crud.py
+++ app/db/crud.py
@@ -8,9 +8,14 @@ from .database import AsyncIOMotorClient
 from config import DB_NAME
 
 
+async def create_new_item(conn: AsyncIOMotorClient, collection: str, data: dict) -> dict:
+    item = await conn[DB_NAME][collection].insert_one(data)
+    return await conn[DB_NAME][collection].find_one({'_id': item.inserted_id})
+
+
 async def retrieve_from_collections(conn: AsyncIOMotorClient, collection: str) -> list[dict]:
     objects = []
-    async for obj in conn[DB_NAME][collection].findall():
+    async for obj in conn[DB_NAME][collection].find():
         objects.append(obj)
     return objects
 
@@ -23,3 +28,15 @@ async def retrieve_data(conn: AsyncIOMotorClient, id: str, collection: str) -> d
 async def get_user_by_email(conn: AsyncIOMotorClient, email: str) -> Optional[UserSchema]:
     obj = await conn[DB_NAME]['user'].find_one({'email': email})
     return UserSchema(**obj) if obj else None
+
+
+async def update_main_page(conn: AsyncIOMotorClient, data: dict) -> bool:
+    item = await conn[DB_NAME]['main_page'].find_one()
+    if item:
+        updated_item = await conn[DB_NAME]['main_page'].update_one({'_id': item.get('_id')},
+                                                                   {'$set': data})
+    else:
+        updated_item = await create_new_item(conn, 'main_page', data)
+    if updated_item:
+        return True
+    return False
--- app/routers/admin.py
+++ app/routers/admin.py
@@ -4,6 +4,9 @@ from fastapi.responses import HTMLResponse
 from app.template_file import templates
 
 from dependencies import check_is_authenticated
+from db.database import AsyncIOMotorClient, get_database
+from db.schema import MainPageSchema
+from db.crud import update_main_page
 
 
 router = APIRouter(
@@ -15,4 +18,11 @@ router = APIRouter(
 
 @router.get('/', response_class=HTMLResponse)
 async def admin_page(request: Request):
-    return templates.TemplateResponse('/admin/admin_index.html', {'request': request})
+    return templates.TemplateResponse('/admin/main_page.html', {'request': request})
+
+
+@router.post('/')
+async def main_page(request: Request, data: MainPageSchema,
+                    conn: AsyncIOMotorClient = Depends(get_database)):
+    result = await update_main_page(conn, data.dict())
+    return {'status': 200 if result else 404}
