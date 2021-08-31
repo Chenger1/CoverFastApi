ac2188c7479ad2797231cb8e974b0bce52439d74
Chenger1 <exs2199@gmail.com>
2021-08-31 12:32:06 +0300

    Contacts page and public handler

--- app/routers/main.py
+++ app/routers/main.py
@@ -4,16 +4,30 @@ from fastapi.responses import HTMLResponse
 from app.template_file import templates
 
 from db.database import AsyncIOMotorClient, get_database
-from db.crud import get_main_page
+from db.crud import get_singleton
 
 router = APIRouter()
 
 
 @router.get('/', response_class=HTMLResponse)
 async def main_page(request: Request, conn: AsyncIOMotorClient = Depends(get_database)):
-    data = await get_main_page(conn)
+    data = await get_singleton(conn, 'main_page')
     return templates.TemplateResponse('index.html',
                                       {'request': request,
                                        'page_name': data.get('page_name'),
                                        'title': data.get('title'),
                                        'text': data.get('text')})
+
+
+@router.get('/contacts', response_class=HTMLResponse)
+async def contacts_page(request: Request, conn: AsyncIOMotorClient = Depends(get_database)):
+    data = await get_singleton(conn, 'contacts')
+    context = {'request': request}
+    if data:
+        context.update(
+            {'phone_number': data.get('phone_number'),
+             'email': data.get('email'),
+             'address': data.get('address')
+             }
+        )
+    return templates.TemplateResponse('contact.html', context)
